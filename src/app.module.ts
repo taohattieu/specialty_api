d303ef14513d6ba070c15672d81f5794fb14b116
Admin <vantaodz1@gmail.com>
2024-04-12 17:21:12 +0700

    addNotification

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProvinceModule } from './province/province.module';
 import { SpecialtyModule } from './specialty/specialty.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [ProvinceModule, SpecialtyModule],
+  imports: [ProvinceModule, SpecialtyModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
