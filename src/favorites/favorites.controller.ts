4668675333e2f31d48e8c39b4fdad3dc0d2816d1
Admin <vantaodz1@gmail.com>
2024-05-13 03:31:47 +0700

    ...

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -5,11 +5,11 @@ import { FavoritesService } from './favorites.service';
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
-//   @Post()
-//   async addToFavorites(
-//     @Body('account_id') account_id: string,
-//     @Body('specialty_id') specialty_id: string,
-//   ) {
-//     return await this.favoritesService.addToFavorites(account_id, specialty_id);
-//   }
+  // @Post()
+  // async addToFavorites(
+  //   @Body('account_id') account_id: string,
+  //   @Body('specialty_id') specialty_id: string,
+  // ) {
+  //   return await this.favoritesService.addToFavorites(account_id, specialty_id);
+  // }
 }
